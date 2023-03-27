7cfad09bed72c2af8dff2e9c683bcbfc7d1b99c3
Hyohwak Lee <crewe1746@naver.com>
2023-03-27 09:26:21 +0900

    Remove time field for logging on server interceptor

--- internal/todo/server/interceptor.go
+++ internal/todo/server/interceptor.go
@@ -3,35 +3,32 @@ package server
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"golang.org/x/exp/slog"
 	"google.golang.org/grpc"
 )
 
 func TodoServerUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	slog.Info("Request received", "method", info.FullMethod, "time", time.Now().Format(time.RFC3339))
+	slog.Info("Request received", "method", info.FullMethod)
 
 	resp, err := handler(ctx, req)
-
 	if err != nil {
-		slog.Error("Request failed", "method", info.FullMethod, "error", err, "time", time.Now().Format(time.RFC3339))
+		slog.Error("Request failed", "method", info.FullMethod, "error", err)
 		return resp, err
 	}
 
-	slog.Info("Request succeeded", "method", info.FullMethod, "type", fmt.Sprintf("%T", resp), "time", time.Now().Format(time.RFC3339))
+	slog.Info("Send a message", "type", fmt.Sprintf("%T", resp))
 	return resp, nil
 }
 
 func TodoServerStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	slog.Info("Request received", "method", info.FullMethod, "time", time.Now().Format(time.RFC3339))
+	slog.Info("Request received", "method", info.FullMethod)
 
 	wss := newWrappedServerStream(ss)
 
 	err := handler(srv, wss)
-
 	if err != nil {
-		slog.Error("Request failed", "method", info.FullMethod, "error", err, "time", time.Now().Format(time.RFC3339))
+		slog.Error("Request failed", "method", info.FullMethod, "error", err)
 		return err
 	}
 
@@ -47,11 +44,11 @@ func newWrappedServerStream(ss grpc.ServerStream) grpc.ServerStream {
 }
 
 func (w *wrappedServerStream) SendMsg(m interface{}) error {
-	slog.Info("Send a message", "type", fmt.Sprintf("%T", m), "time", time.Now().Format(time.RFC3339))
+	slog.Info("Send a message", "type", fmt.Sprintf("%T", m))
 	return w.ServerStream.SendMsg(m)
 }
 
 func (w *wrappedServerStream) RecvMsg(m interface{}) error {
-	slog.Info("Receive a message", "type", fmt.Sprintf("%T", m), "time", time.Now().Format(time.RFC3339))
+	slog.Info("Receive a message", "type", fmt.Sprintf("%T", m))
 	return w.ServerStream.RecvMsg(m)
 }
