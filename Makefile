523da705345f0626de2af56d56400e384dc87134
Hyohwak Lee <crewe1746@naver.com>
2023-03-28 15:00:07 +0900

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+server:
+	go run ./cmd/server/main.go -p 8080
+
+client:
+	go run ./cmd/client/main.go -p 8080
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"math/rand"
 
 	"github.com/piatoss3612/go-grpc-todo/gen/go/todo/v1"
 	"github.com/piatoss3612/go-grpc-todo/internal/todo/client"
@@ -35,40 +33,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to add todo: %v", err)
 	}
-
-	addStream, err := client.AddMany(context.Background())
-	if err != nil {
-		log.Fatalf("failed to add many todos: %v", err)
-	}
-
-	for i := 0; i < 5; i++ {
-		err := addStream.Send(&todo.AddRequest{Content: fmt.Sprintf("test %d", i), Priority: todo.Priority(rand.Intn(4))})
-		if err != nil {
-			log.Fatalf("failed to add many todos: %v", err)
-		}
-	}
-	log.Printf("added todo with id: %s\n", id.Id)
-
-	item, err := client.Get(context.Background(), &todo.GetRequest{Id: id.Id})
-	if err != nil {
-		log.Fatalf("failed to get todo: %v", err)
-	}
-
-	log.Printf("got todo: %v\n", item)
-
-	getStream, err := client.GetAll(context.Background(), &todo.Empty{})
-	if err != nil {
-		log.Fatalf("failed to get all todos: %v", err)
-	}
-
-	for {
-		item, err := getStream.Recv()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatalf("failed to get all todos: %v", err)
-		}
-		log.Printf("got todo: %v\n", item)
-	}
+	log.Printf("added todo with id: %v", id)
 }
