4a61686c97256ad44b3346b02d6a145046896b1c
Hyohwak Lee <crewe1746@naver.com>
2023-03-30 17:32:36 +0900

    Refactor postgresTodos

--- internal/repository/postgres/todos.go
+++ internal/repository/postgres/todos.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/piatoss3612/go-grpc-todo/gen/go/todo/v1"
 	"github.com/piatoss3612/go-grpc-todo/internal/repository"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type postgresTodos struct {
@@ -27,31 +28,45 @@ func (p *postgresTodos) BeginTx(ctx context.Context, opts ...*sql.TxOptions) (*s
 
 func (p *postgresTodos) Add(ctx context.Context, content string, prior todo.Priority, txs ...*sql.Tx) (string, error) {
 	id := uuid.New().String()
-	stmt := `INSERT INTO todos (content, priority) VALUES ($1, $2)`
+	stmt := `INSERT INTO todos (id, content, priority) VALUES ($1, $2, $3)`
+
+	var err error
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		_, err := tx.ExecContext(ctx, stmt, content, prior)
-		return id, err
+		_, err = tx.ExecContext(ctx, stmt, id, content, prior)
+	} else {
+		_, err = p.db.ExecContext(ctx, stmt, id, content, prior)
 	}
 
-	_, err := p.db.ExecContext(ctx, stmt, content, prior)
-	return id, err
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
 }
 
 func (p *postgresTodos) Get(ctx context.Context, id string, txs ...*sql.Tx) (*todo.Todo, error) {
 	query := `SELECT id, content, priority, is_done, created_at, updated_at FROM todos WHERE id = $1`
 
 	var t todo.Todo
+	var createdAt, updatedAt time.Time
+	var err error
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		err := tx.QueryRowContext(ctx, query, id).Scan(&t.Id, &t.Content, &t.Priority, &t.IsDone, &t.CreatedAt, &t.UpdatedAt)
-		return &t, err
+		err = tx.QueryRowContext(ctx, query, id).Scan(&t.Id, &t.Content, &t.Priority, &t.IsDone, &createdAt, &updatedAt)
+	} else {
+		err = p.db.QueryRowContext(ctx, query, id).Scan(&t.Id, &t.Content, &t.Priority, &t.IsDone, &createdAt, &updatedAt)
 	}
 
-	err := p.db.QueryRowContext(ctx, query, id).Scan(&t.Id, &t.Content, &t.Priority, &t.IsDone, &t.CreatedAt, &t.UpdatedAt)
-	return &t, err
+	if err != nil {
+		return nil, err
+	}
+
+	t.CreatedAt = timestamppb.New(createdAt)
+	t.UpdatedAt = timestamppb.New(updatedAt)
+	return &t, nil
 }
 
 func (p *postgresTodos) GetAll(ctx context.Context, txs ...*sql.Tx) ([]*todo.Todo, error) {
@@ -59,30 +74,21 @@ func (p *postgresTodos) GetAll(ctx context.Context, txs ...*sql.Tx) ([]*todo.Tod
 
 	var todos []*todo.Todo
 
+	var rows *sql.Rows
+	var err error
+
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		rows, err := tx.QueryContext(ctx, query)
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var t todo.Todo
-			err := rows.Scan(&t.Id, &t.Content, &t.Priority, &t.IsDone, &t.CreatedAt, &t.UpdatedAt)
-			if err != nil {
-				return nil, err
-			}
-			todos = append(todos, &t)
-		}
-		return todos, nil
+		rows, err = tx.QueryContext(ctx, query)
+	} else {
+		rows, err = p.db.QueryContext(ctx, query)
 	}
 
-	rows, err := p.db.QueryContext(ctx, query)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
+
+	defer func() { _ = rows.Close() }()
 
 	for rows.Next() {
 		var t todo.Todo
@@ -95,41 +101,62 @@ func (p *postgresTodos) GetAll(ctx context.Context, txs ...*sql.Tx) ([]*todo.Tod
 	return todos, nil
 }
 
-func (p *postgresTodos) Update(ctx context.Context, id string, content string, prior todo.Priority, done bool, txs ...*sql.Tx) error {
+func (p *postgresTodos) Update(ctx context.Context, id string, content string, prior todo.Priority, done bool, txs ...*sql.Tx) (int64, error) {
 	stmt := `UPDATE todos SET content = $1, priority = $2, is_done = $3, updated_at = $4 WHERE id = $5`
 
+	var res sql.Result
+	var err error
+
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		_, err := tx.ExecContext(ctx, stmt, content, prior, done, time.Now(), id)
-		return err
+		res, err = tx.ExecContext(ctx, stmt, content, prior, done, time.Now(), id)
+	} else {
+		res, err = p.db.ExecContext(ctx, stmt, content, prior, done, time.Now(), id)
 	}
 
-	_, err := p.db.ExecContext(ctx, stmt, content, prior, done, time.Now(), id)
-	return err
+	if err != nil {
+		return 0, err
+	}
+
+	return res.RowsAffected()
 }
 
-func (p *postgresTodos) Delete(ctx context.Context, id string, txs ...*sql.Tx) error {
+func (p *postgresTodos) Delete(ctx context.Context, id string, txs ...*sql.Tx) (int64, error) {
 	stmt := `DELETE FROM todos WHERE id = $1`
 
+	var res sql.Result
+	var err error
+
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		_, err := tx.ExecContext(ctx, stmt, id)
-		return err
+		res, err = tx.ExecContext(ctx, stmt, id)
+	} else {
+		res, err = p.db.ExecContext(ctx, stmt, id)
 	}
 
-	_, err := p.db.ExecContext(ctx, stmt, id)
-	return err
+	if err != nil {
+		return 0, err
+	}
+
+	return res.RowsAffected()
 }
 
-func (p *postgresTodos) DeleteAll(ctx context.Context, txs ...*sql.Tx) error {
+func (p *postgresTodos) DeleteAll(ctx context.Context, txs ...*sql.Tx) (int64, error) {
 	stmt := `DELETE FROM todos`
+
+	var res sql.Result
+	var err error
 
 	if len(txs) > 0 && txs[0] != nil {
 		tx := txs[0]
-		_, err := tx.ExecContext(ctx, stmt)
-		return err
+		res, err = tx.ExecContext(ctx, stmt)
+	} else {
+		res, err = p.db.ExecContext(ctx, stmt)
 	}
 
-	_, err := p.db.ExecContext(ctx, stmt)
-	return err
+	if err != nil {
+		return 0, err
+	}
+
+	return res.RowsAffected()
 }
--- internal/repository/todos.go
+++ internal/repository/todos.go
@@ -12,7 +12,7 @@ type Todos interface {
 	Add(ctx context.Context, content string, prior todo.Priority, txs ...*sql.Tx) (string, error)
 	Get(ctx context.Context, id string, txs ...*sql.Tx) (*todo.Todo, error)
 	GetAll(ctx context.Context, txs ...*sql.Tx) ([]*todo.Todo, error)
-	Update(ctx context.Context, id string, content string, prior todo.Priority, done bool, txs ...*sql.Tx) error
-	Delete(ctx context.Context, id string, txs ...*sql.Tx) error
-	DeleteAll(ctx context.Context, txs ...*sql.Tx) error
+	Update(ctx context.Context, id string, content string, prior todo.Priority, done bool, txs ...*sql.Tx) (int64, error)
+	Delete(ctx context.Context, id string, txs ...*sql.Tx) (int64, error)
+	DeleteAll(ctx context.Context, txs ...*sql.Tx) (int64, error)
 }
