9cff1e801f44699377cc318d4234d84c493c7f5c
Hyohwak Lee <crewe1746@naver.com>
2023-04-04 22:31:31 +0900

    Remove broker directory

--- internal/broker/consumer.go
+++
@@ -1,6 +0,0 @@
-package broker
-
-type EventConsumer interface {
-	Consume(topics []string, sig <-chan bool) (<-chan Event, <-chan error, error)
-	Close() error
-}
--- internal/broker/producer.go
+++
@@ -1,7 +0,0 @@
-package broker
-
-type EventProducer interface {
-	Produce(event Event) error
-	DeliveryReport() (<-chan string, <-chan error)
-	Close() error
-}
